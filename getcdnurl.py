5d72c12b780de4f151cc7f13dc7351f5fd2e4f97
Benjamin Wölkchen <42138517+benjaminwolkchen@users.noreply.github.com>
2020-06-07 20:45:20 +0200

    Create getcdnurl.py

---
+++ getcdnurl.py
@@ -0,0 +1,20 @@
+#!/usr/python
+
+# URL without the protocol. (HTTP/HTTPS)
+url = ""
+options = "?ssl=1"
+
+i0 = "https://i0.wp.com/"
+i1 = "https://i1.wp.com/"
+i2= "https://i2.wp.com/"
+i3 = "https://i3.wp.com/"
+
+url0 = i0 + url + options
+url1 = i2 + url + options
+url2 = i3 + url + options
+url3 = i4 + url + options
+
+print(url0)
+print(url1)
+print(url2)
+print(url3)
